3f99ca283e7a7c020862b5ae2cc8be31daaf6212
NezhelskiyViktor <nezhelskiy@yandex.ru>
2024-04-26 14:15:31 +0300

    Доработан игровой процесс и файл README.

--- main.py
+++ main.py
@@ -8,24 +8,37 @@ from random import choice
 
 # добавляем ещё один тип оружия - копьё
 class Spear(Weapon):
-    def attack(self):
-        print("Вонзает копьё и ...")
+    def attack(self, monster_name):
+        print("вонзает копьё в ", monster_name, end=".\n")
 
 
+print("На русские земли пришла беда!\n Напали:")
 # создаем игровые объекты
-monster = Monster()
-fighters = ["Илья Муромец", "Добрыня Никитич", "Алёша Попович"] # создаем список бойцов
+monsters = ["Змей горыныч", "Чудище морское", "Ведьмак кровавый", "Скелет костлявый"]  # создаем список монстров
+fighters = ["Илья Муромец", "Добрыня Никитич", "Алёша Попович"]  # создаем список бойцов
 arsenal = [Bow(), Sword(), Spear()]  # создаем арсенал оружия
+
+gang_of_monsters = [Monster(name) for name in monsters]  # создаем армию монстров
+print("\nРусские воины вышли на бой с армой монстров.")
+armi_of_fighters = [Fighter(name) for name in fighters]  # создаем армию бойцов
+
+for fighter in armi_of_fighters:
+    weapon = arsenal.pop()
+    fighter.set_weapon(weapon)  # вооружаем бойца
 
 
 # создаем игровой метод
-def game(fighter_name):
-    new_fighter = Fighter(fighter_name)  # создаем объект бойца идущего в бой
-    weapon = choice(arsenal)  # выбираем ему оружие
-    new_fighter.change_weapon(weapon)  # вооружаем бойца
-    new_fighter.weapon.attack()  # боец атакует
-    monster.attack()  # монстр реагирует на атаку
+def game(_fighter):
+    if len(gang_of_monsters) > 0:
+        monster = choice(gang_of_monsters)  # выбираем случайного монстра
+        if monster.get_dead():  # если монстр убит
+            gang_of_monsters.remove(monster)  # удаляем его из армии
+        else:
+            _fighter.get_weapon().attack(monster.name)  # боец атакует
+            monster.reacts()  # монстр реагирует на атаку
 
 
-for fighter in fighters:  # в порядке очереди
-    game(fighter)  # бойцы идут в бой
+print("\nИ вот бой начинается!")
+while gang_of_monsters:  # пока армия монстров не пуста
+    for fight in armi_of_fighters:  # все бойцы в проядке очереди
+        game(fight)  # идут в бой
--- my_clases.py
+++ my_clases.py
@@ -1,3 +1,4 @@
+import random
 from abc import ABC, abstractmethod
 from random import choice
 
@@ -7,34 +8,50 @@ class Fighter:  # класс `Fighter`, представляющий бойца.
     # Шаг 3: Модифицируйте класс `Fighter`
     def __init__(self, name):
         self.name = name
-        self.weapon = None  # добавили параметр `weapon`, для хранения объект класса `Weapon`.
-        print(f"И вот на бой выходит {self.name}.") # созданный боец заявляет о себе
+        self.__weapon = None  # добавили параметр `weapon`, для хранения объект класса `Weapon`.
+        print(self.name)  # созданный боец заявляет о себе
 
-# добавили метод `changeWeapon()`, который позволяет изменить оружие бойца.
-    def change_weapon(self, weapon):
-        self.weapon = weapon
+    # добавили метод `changeWeapon()`, который позволяет изменить оружие бойца.
+    def set_weapon(self, weapon):
+        self.__weapon = weapon
+
+    def get_weapon(self):
+        print(self.name, end=" ")
+        return self.__weapon
 
 
 class Monster:  # класс `Monster`, представляющий монстра.
-    def attack(self):
-        print("монстр " + choice(["ренен.", "падает замертво.", "отступает."]))
+    def __init__(self, name="Монстр"):
+        self.name = name
+        self.__dead = False
+        print(self.name)
+
+    def get_dead(self):
+        return self.__dead
+
+    def reacts(self):
+        self.__dead = random.randint(1, 8) == 8
+        if self.__dead:
+            print(f"{self.name} убит.\n Ура!!!")
+        else:
+            print(f"{self.name} {choice(["ренен.", "атакует.", "отступает."])}")
 
 
 # Шаг 1:Создайте абстрактный класс для оружия
 # Создаем абстрактный класс `Weapon`, который содержит абстрактный метод `attack()`.
 class Weapon(ABC):  # абстрактный класс `Weapon`
     @abstractmethod
-    def attack(self):  # абстрактный метод `attack()`
+    def attack(self, target):  # абстрактный метод `attack()`
         pass
 
 
 # Шаг 2: Реализуйте конкретные типы оружия
 # Каждый из этих классов реализует метод `attack()` своим уникальным способом.
 class Sword(Weapon):
-    def attack(self):
-        print("Бьет мечом и ...")
+    def attack(self, monster_name):
+        print("бьет мечом в ", monster_name, end=".\n")
 
 
 class Bow(Weapon):
-    def attack(self):
-        print("Стреляет из лука и ...")
+    def attack(self, monster_name):
+        print("стреляет из лука в ", monster_name, end=".\n")
